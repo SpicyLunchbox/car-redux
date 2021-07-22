347fb8444afe7b7dd19354e013515187ca9a9c43
SpicyLunchbox <westonwoodard28@gmail.com>
2021-07-22 08:53:14 -0700

    can now add each feature only once

--- src/components/AdditionalFeature.js
+++ src/components/AdditionalFeature.js
@@ -1,14 +1,21 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {connect} from 'react-redux';
 import {addFeature} from '../store/actions';
 
 const AdditionalFeature = (props) => {
+  const [isVisible, setIsVisible] = useState(false)
+
+  const createElement = () => {
+    setIsVisible(true)
+  }
 
   return (
     <li>
-      <button className="button" onClick={() => props.addFeat(props.feature)}>Add</button>
+      <button className="button" onClick={() => props.car.features.includes(props.feature)? createElement():props.addFeat(props.feature)}>Add</button>
       {props.feature.name} (+{props.feature.price})
+      {isVisible && <p className="feature-check">Feature already added</p>}
     </li>
+    
   );
 };
 
