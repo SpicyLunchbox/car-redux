ab2b3a46e8ac45cb41cd6cce98824b813156f5bf
SpicyLunchbox <westonwoodard28@gmail.com>
2021-07-22 07:51:42 -0700

    boilerplate complete. reducer created

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Header from './components/Header';
+import AddedFeatures from './components/AddedFeatures';
+import AdditionalFeatures from './components/AdditionalFeatures';
+import Total from './components/Total';
 
-function App() {
+const App = () => {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="boxes">
+      <div className="box">
+        <Header/>
+        <AddedFeatures/>
+      </div>
+      <div className="box">
+        <AdditionalFeatures/>
+        <Total/>
+      </div>
     </div>
-  );
-}
+  )
+};
 
 export default App;
